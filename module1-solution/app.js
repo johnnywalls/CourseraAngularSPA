c540ca1fe6b77bd49f64f89c91cb2e2fdf9c1060
Juan Paredes <juan.paredes@gmail.com>
2016-12-24 20:42:41 +0100

    Solution for Assignment 1

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,16 +8,16 @@ LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController($scope) {
   $scope.checkLunchMenu = function() {
-    // split menu string and check for empty strings =)
     var menu = $scope.lunchMenu;
-    var data_error_message = 'Please enter data first';
+    var errorMessage = 'Please enter data first';
     if ( menu === undefined || menu == '' ) {
-      $scope.showMessage( data_error_message, 'danger' );
+      $scope.showMessage( errorMessage, 'danger' );
     }
     else {
+      // split menu string and check for empty strings at once :)
       var menuItems = menu.split(',').filter(v => v.trim()!='');
       if (menuItems.length == 0) {
-        $scope.showMessage( data_error_message, 'danger' );
+        $scope.showMessage( errorMessage, 'danger' );
       }
       else if (menuItems.length <= 3) {
         $scope.showMessage( 'Enjoy!', 'success' );
@@ -30,7 +30,7 @@ function LunchCheckController($scope) {
 
   $scope.showMessage = function ( text, type ){
     $scope.message = text;
-    $scope.contextClass = 'alert alert-'+type;
+    $scope.contextClass = 'alert alert-'+type; // use bootstrap context classes
   };
 }
 
